318798eb7ac4e8188384cdbe2fd38883af5ddc9a
devchoi89 <devchoi89@gmail.com>
2020-07-04 19:28:38 +0900

    Updates

---
+++ precache-manifest.e0c3fbfd460abe12ea4d6901d0d778b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4e9dba7962d894376a206747e712462",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "cbf4a33c7f7560d40cce",
+    "url": "/movie_app_2020/static/css/main.f5c1876a.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf4a33c7f7560d40cce",
+    "url": "/movie_app_2020/static/js/main.9e014827.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
